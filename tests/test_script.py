262d024511190cfea26f93d21b1b45dc0bf38b24
eyerus21 <eyugher21@gmail.com>
2021-07-16 06:16:09 +0300

    Update

--- tests/test_script.py
+++ tests/test_script.py
@@ -2,7 +2,7 @@ import unittest
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
 
-from scripts.script import find_average, count_occurence
+from Scripts.script import find_average, count_occurence
 
 
 class TestCases(unittest.TestCase):
