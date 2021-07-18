27c90023cc2452d63244ac427ea90643f0d15b49
eyerus21 <eyugher21@gmail.com>
2021-07-19 01:36:37 +0300

    make it better

--- notebooks/add.py
+++ notebooks/add.py
@@ -60,3 +60,58 @@ top10= top10['IMSI'].nunique().nlargest(10)
 y=df[['Handset Manufacturer','IMSI']].groupby(['Handset Manufacturer'])['IMSI'].nunique().nlargest(3)
 #Apple
 df[df['Handset Manufacturer']=='Apple'].groupby(['Handset Type'])['IMSI'].nunique().nlargest(5).plot(kind='barh')
+df[df['Handset Manufacturer']=='Samsung'].groupby(['Handset Type'])['IMSI'].nunique().nlargest(5)
+df[df['Handset Manufacturer']=='Huawei'].groupby(['Handset Type'])['IMSI'].nunique().nlargest(5)
+
+# Aggregating per user the number of xDR session column
+df_session_count = df.groupby(['MSISDN/Number'])['Bearer Id'].agg(['count'])
+df_session_count.head(10)
+
+# Aggregating per user the Session duration column
+# Aggregating per user the Session duration column
+df_session_duration = df.groupby(['MSISDN/Number'])['Dur. (ms)'].agg(['sum'])
+df_session_duration.head(10)
+
+#Total Data Uploaded and Downloaded¶
+# adding two columns and forming a new column
+def adding_columns(name,column1,column2):
+    df[name] = df_clean[column1] + df_clean[column2]
+    return df
+adding_columns(name='Total_UL_DL',column1='Total UL (Bytes)',column2='Total DL (Bytes)')
+adding_columns(name='Avg_RTT_UL_DL',column1='Avg RTT DL (ms)',column2='Avg RTT UL (ms)')
+adding_columns(name='TCP_UL_DL_Retrans. Vol_(Bytes)',column1='TCP DL Retrans. Vol (Bytes)',column2='TCP UL Retrans. Vol (Bytes)')
+adding_columns(name='Avg_ Bearer_TP DL_UL _(kbps)',column1='Avg Bearer TP DL (kbps)',column2='Avg Bearer TP UL (kbps)')
+adding_columns(name='Social_Media_UL_DL',column1='Social Media DL (Bytes)',column2='Social Media UL (Bytes)')
+adding_columns(name='Google_UL_DL',column1='Google DL (Bytes)',column2='Google UL (Bytes)')
+adding_columns(name='Email_UL_DL',column1='Email DL (Bytes)',column2='Email DL (Bytes)')
+adding_columns(name='Youtube_UL_DL',column1='Youtube DL (Bytes)',column2='Youtube DL (Bytes)')
+adding_columns(name='Netflix_UL_DL',column1='Netflix DL (Bytes)',column2='Netflix UL (Bytes)')
+adding_columns(name='Gaming_UL_DL',column1='Gaming DL (Bytes)',column2='Gaming UL (Bytes)')
+adding_columns(name='Other_UL_DL',column1='Other DL (Bytes)',column2='Other UL (Bytes)')
+
+# Aggregating per user the Total_UL_DL column
+
+user_grp1 = df.groupby(['MSISDN/Number'])
+user_grp1['Total_UL_DL'].agg(['min','max','mean','median','sum']).head(10)
+
+
+# Aggregating per user the Social_Media_UL_DL column
+
+user_grp2 = df.groupby(['MSISDN/Number'])
+user_grp2['Social_Media_UL_DL'].agg(['min','max','mean','median','sum']).head(10)
+
+# Aggregating per user the Google_UL_DL column
+
+user_grp3 = df.groupby(['MSISDN/Number'])
+user_grp3['Google_UL_DL'].agg(['min','max','mean','median','sum']).head(10)
+
+# Aggregating per user the Email_UL_DL column
+
+user_grp4 = df.groupby(['MSISDN/Number'])
+user_grp4['Email_UL_DL'].agg(['min','max','mean','median','sum']).head(10)
+
+# Aggregating per user the Youtube_UL_DL column
+
+user_grp5= df.groupby(['MSISDN/Number'])
+user_grp5['Youtube_UL_DL'].agg(['min','max','mean','median','sum']).head(10)
+
